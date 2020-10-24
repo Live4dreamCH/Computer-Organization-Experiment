29ef6880034d31976480e99d8c410a89161d601f
Live4dreamCH <live4dream@stu.xjtu.edu.cn>
2020-10-24 17:15:23 +0800

    D触发器及其测试

--- exp_2/exp_2.srcs/sources_1/new/D_flip_flop.v
+++ exp_2/exp_2.srcs/sources_1/new/D_flip_flop.v
@@ -19,13 +19,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module D_flip_flop(
-    input D,
-    input en,
-    input clk,
-    input clear,
-    input Q,
-    input NQ
+    D,
+    en,     //en是高电平有效的使能
+    clk,    //clk是上升沿触发的时钟信号
+    clear,  //clear,set->Q;
+    set,    //1,1->x; 1,0->0; 0,1->1; 0,0->D;
+    Q,
+    NQ      //Q与NQ是一对互反的输出, Q跟随D
     );
+    //D触发器    
+    input D, en, clk, clear, set;
+    output Q, NQ;
+    reg Q, NQ;
+
+    always @(posedge clk) begin
+        if (en) begin
+            case ({clear, set})
+                2'b11: Q = 1'bx;
+                2'b10: Q = 1'b0;
+                2'b01: Q = 1'b1;
+                default: Q = D;
+            endcase
+            NQ = ~Q;
+        end
+    end
 endmodule
